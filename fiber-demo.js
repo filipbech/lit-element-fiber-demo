344b2afda2f86a3626d2920c38dc26b34c19b648
Filip Bech-Larsen <filipbech@filipbech.dk>
2018-01-17 10:10:37 +0100

    update

--- fiber-demo.js
+++ fiber-demo.js
@@ -3,6 +3,10 @@ import { LitElement, html } from './node_modules/lit-html-element/lit-element.js
 export class FiberDemo extends LitElement {
     static get properties() {
         return {
+            elapsed: {
+                type: Number,
+                value: 0
+            },
             seconds: {
                 type: Number,
                 value: 0
@@ -12,48 +16,41 @@ export class FiberDemo extends LitElement {
 
     connectedCallback() {
         super.connectedCallback();
-        this.shouldUpdate = true;
         this.seconds = 0;
         var tick = this.tick.bind(this);
-        this.intervalID = setInterval(tick, 5000);
+        this.intervalID = setInterval(tick, 1000);
     }
-   
 
-  tick() {
-    this.shouldUpdate = true;
-    this.seconds = (this.seconds % 10) + 1;
-  }
-
-  render() {
-    if(this.shouldUpdate) {
-        this.shouldUpdate = false;
-        this.cachedTemplate = html`
-            <link rel="stylesheet" href="style.css">
-            <div>
-                <fiber-triangle x="${0}" y="${0}" s="${1000}" seconds="${this.seconds}"></fiber-triangle>
-            </div>
-        `;
+    tick() {
+        this.seconds = (this.seconds % 10) + 1;
     }
-    return this.cachedTemplate;
-  }
 
+    renderCallback() {
+        const t = (this.elapsed / 1000) % 10;
+        const scale = 1 + (t > 5 ? 10 - t : t) / 10;
+        this.style.transform = `scaleX(${scale / 2.1}) scaleY(0.7) translateZ(0.1px)`;
 
-constructor() {
-    super();
-    this._elapsed=0;
-    Object.defineProperty(this, 'elapsed', {
-        get() { return this._elapsed },
-        set(v) {
-            this._elapsed = v;
+        super.renderCallback();
+    }
 
-            const t = (this.elapsed / 1000) % 10;
-            const scale = 1 + (t > 5 ? 10 - t : t) / 10;
-            this.style.transform = `scaleX(${scale / 2.1}) scaleY(0.7) translateZ(0.1px)`;
+    render({ seconds }) {
+        return html`
+      <style>
+        :host {
+          position: absolute;
+          transform-origin: 0 0;
+          left: 50%;
+          top: 50%;
+          width: 10px;
+          height: 10px;
+          background: #eee;
         }
-    });
-}
+      </style>
 
-
+      <fiber-triangle x="${0}" y="${0}" s="${1000}" seconds="${seconds}">
+      </fiber-triangle>
+    `;
+    }
 }
 
 customElements.define('fiber-demo', FiberDemo.withProperties());
